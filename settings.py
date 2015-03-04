fef04446795aa165ffbc32b957ed65c0aa2ac4a8
Jacob Bottelberghe <j.bottelberghe@gmail.com>
2015-03-04 13:22:04 -0700

    Select Kodi environment based on network connection
    This is primarily just to hasten development from the
    developer's standpoint, however in the future we will
    likely change settings.py into a default settings environment
    and then import other information in a "local_settings"
    which will be ignored in version control.

--- settings.py
+++ settings.py
@@ -1,1 +1,10 @@
+import commands
+
+DEBUG = True
 KODI_URI = 'http://192.168.1.2:8080'
+
+if DEBUG:
+    # If we are connected to BLUEZONE, assume that we wish to use our local Kodi installation
+    output = commands.getstatusoutput('nmcli con status')
+    if 'BLUEZONE' in output[1]:
+        KODI_URI = 'http://localhost:8080'
