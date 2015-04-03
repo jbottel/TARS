jbottel/TARS

commit 2d9c03a398a1213e6f8d09c291cca5ed2854fee2

Display TV show title near progress bar
We take the currently playing item from the get_properties call as added
in dd5f89b and do further processing in JavaScript to display the title.

The items are truncated at a length of 22 characters, otherwise the
footer bar will need to grow to accommodate the size. The user can
rollover and read the full title if necessary.

Makes progress on #14, however it will only work for TV Shows and should
work across the board for all types of media.

static/js/main.js
@@ -69,6 +69,18 @@ $(document).ready(function() {
 
 	function updateSlider() {
 		$.getJSON('/get_properties','',function(data) {
+			if (data.item.type == "episode") {
+				var tvShowTitle = data.item.showtitle;
+				var displayTVShowTitle = tvShowTitle;
+				if (tvShowTitle.length > 22) displayTVShowTitle = tvShowTitle.substring(0,22) + "...";
+				var showTitleHTML = "<span title='" + tvShowTitle + "'>" + displayTVShowTitle + "</span>";
+				$("#tv-show-title").html(showTitleHTML);
+				var episodeTitle = "S" + data.item.season + "E" + data.item.episode + " - " + data.item.title;
+				var displayTitle = episodeTitle;
+				if (episodeTitle.length > 21) displayTitle = episodeTitle.substring(0,21) + "...";
+				var titleHTML = "<span title='" + episodeTitle + "'>" + displayTitle + "</span>";
+				$("#episode-title").html(titleHTML);
+			}
 			if (data.speed == 0) {
 			$("#play-pause-button").removeClass("glyphicon-pause");
 			$("#play-pause-button").addClass("glyphicon-play");

TARS.py
@@ -406,7 +406,7 @@ def get_properties():
     try:
         player_properties = xbmc.Player.GetProperties({"playerid":1,"properties":["time","percentage","totaltime","speed"]})["result"]
         app_properties = xbmc.Application.GetProperties({"properties":["volume","muted"]})["result"]
-        playing_properties = xbmc.Player.GetItem({"playerid":1,"properties":["title","season","episode","showtitle","thumbnail","streamdetails"]})["result"]
+        playing_properties = xbmc.Player.GetItem({"playerid":1,"properties":["title","season","episode","showtitle","thumbnail"]})["result"]
         properties = dict(player_properties.items() + app_properties.items() + playing_properties.items())
     except:
         properties = {"error":True}

templates/base.html
@@ -60,7 +60,7 @@
 									<div class="navbar-collapse collapse" id="footer-body">
 										<div class="nav navbar-nav navbar-left" style="line-height: 50px;">
 											<div style="float: left; vertical-align: middle; height:50px;">
-												<div style="vertical-align: middle; line-height: 20px;font-size: 90%; padding-right: 30px; padding-top:5px;">Title of the TV Show<br>Title of the Episode</div>
+												<div style="vertical-align: middle; line-height: 20px;font-size: 90%; padding-right: 30px; padding-top:5px;"><span id="tv-show-title">Title of the TV Show</span><br><span id="episode-title">Title of the Episode</span></div>
 											</div>
 											<div id="controls-container">
 												<a style="width: 20px;"><span id="stop-button" class="glyphicon glyphicon-stop" aria-hidden="true"></span></a>&nbsp;

commit 2f31ccc73ef1e8b920779cf453379953a933cfaf

Fix bug where progress doesn't update after stop
We fix the bug introduced in 2d9c03a where the progress display doesn't
update after an item stops playing in Kodi.

We also make the item title display disappear when an item stops
playing -- a bug detailed in #19 where an item's title remains onscreen
even after it has stopped.

Resolves #18, #19

static/js/main.js
@@ -69,18 +69,7 @@ $(document).ready(function() {
 
 	function updateSlider() {
 		$.getJSON('/get_properties','',function(data) {
-			if (data.item.type == "episode") {
-				var tvShowTitle = data.item.showtitle;
-				var displayTVShowTitle = tvShowTitle;
-				if (tvShowTitle.length > 22) displayTVShowTitle = tvShowTitle.substring(0,22) + "...";
-				var showTitleHTML = "<span title='" + tvShowTitle + "'>" + displayTVShowTitle + "</span>";
-				$("#tv-show-title").html(showTitleHTML);
-				var episodeTitle = "S" + data.item.season + "E" + data.item.episode + " - " + data.item.title;
-				var displayTitle = episodeTitle;
-				if (episodeTitle.length > 21) displayTitle = episodeTitle.substring(0,21) + "...";
-				var titleHTML = "<span title='" + episodeTitle + "'>" + displayTitle + "</span>";
-				$("#episode-title").html(titleHTML);
-			}
+			
 			if (data.speed == 0) {
 			$("#play-pause-button").removeClass("glyphicon-pause");
 			$("#play-pause-button").addClass("glyphicon-play");
@@ -96,7 +85,25 @@ $(document).ready(function() {
 				pBar.slider('setAttribute','max',100);
 				pBar.slider('setValue',0);
 				$("#secdisp").html("0:00 / 0:00");
+				$("#tv-show-title").html("<span style='padding-right: 11em;'></span>");
+				$("#episode-title").html("&nbsp;");
 				return;
+			}
+			if (data.item.type == "episode") {
+				var tvShowTitle = data.item.showtitle;
+				var displayTVShowTitle = tvShowTitle;
+				if (tvShowTitle.length > 18) displayTVShowTitle = tvShowTitle.substring(0,18) + "...";
+				var showTitleHTML = "<span title='" + tvShowTitle + "'>" + displayTVShowTitle + "</span>";
+				$("#tv-show-title").html(showTitleHTML);
+				var episodeTitle = "S" + data.item.season + "E" + data.item.episode + " - " + data.item.title;
+				var displayTitle = episodeTitle;
+				if (episodeTitle.length > 18) displayTitle = episodeTitle.substring(0,18) + "...";
+				var titleHTML = "<span title='" + episodeTitle + "'>" + displayTitle + "</span>";
+				$("#episode-title").html(titleHTML);
+			}
+			else {
+				$("#tv-show-title").html("");
+				$("#episode-title").html("");
 			}
 
 