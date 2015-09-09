076f1a22ce0642be9ef3d736f98595486136959f
Jacob Bottelberghe <j.bottelberghe@gmail.com>
2015-09-08 19:21:19 -0600

    Test for proper remote functionality in player
    We add some tests to ensure that the most-commonly and most important
    functions in the remote control trigger the correct action on the Kodi
    player.
    
    This requires the tests to have a functioning JSON-RPC connection of its
    own, in order to check the state of the player at each point in the
    test.
    
    A necessary condition for the tests to proceed is that the Kodi library
    contains at least one movie item with the movie_id being 1. This is
    likely the case on most Kodi installations, but any installation with an
    empty movie library will fail these tests. This movie will, of course,
    flash on and off of the Kodi screen while the tests are running.

--- tests.py
+++ tests.py
@@ -54,5 +54,61 @@ class StatusCodeTestCase(unittest.TestCase):
         result = self.c.get('/remote')
         self.assertEqual(result.status_code, 200) 
 
+class RemoteControlTestCase(unittest.TestCase):
+
+    def setUp(self):
+        TARS.app.config['TESTING'] = True
+        self.xbmc = XBMC(settings.JSONRPC_URI + '/jsonrpc')
+        self.c = TARS.app.test_client()
+
+    def tearDown(self):
+        pass
+
+    def test_playpause(self):
+        """Check that the play/pause functionality properly pauses and playes the Kodi player when requested."""
+        # Play the first movie available in the database (will fail if movie database is empty)
+        self.xbmc.Player.Open({'item': {'movieid': 1}})
+
+        # Remote Control: Pause
+        result = self.c.get('/remote/playpause')
+        # Check 'speed' attribute to check
+        player_properties = self.xbmc.Player.GetProperties({"playerid": 1, "properties": ["speed"]})["result"] 
+        self.assertEqual(player_properties['speed'], 0) 
+
+        # Remote Control: Play
+        result = self.c.get('/remote/playpause')
+        # Check 'speed' attribute to check
+        player_properties = self.xbmc.Player.GetProperties({"playerid": 1, "properties": ["speed"]})["result"] 
+        self.assertEqual(player_properties['speed'], 1) 
+
+        self.xbmc.Player.Stop({"playerid": 1})
+
+    def test_stop(self):
+        """Check that the stop functionality properly stops the Kodi player when requested."""
+        # Play the first movie available in the database (will fail if movie database is empty)
+        self.xbmc.Player.Open({'item': {'movieid': 1}})
+
+        # Send stop request
+        result = self.c.get('/remote/stop')
+        
+        # Check active player
+        active = self.xbmc.Player.GetActivePlayers()["result"]
+        self.assertFalse(active) 
+
+    def test_fast_forward(self):
+        """Check that the fast forward functionality properly fast-forwards the Kodi player when requested."""
+        # Play the first movie available in the database (will fail if movie database is empty)
+        self.xbmc.Player.Open({'item': {'movieid': 1}})
+
+        # Send fast forward request
+        result = self.c.get('/remote/fastforward')
+
+        # Check 'speed' attribute to check
+        player_properties = self.xbmc.Player.GetProperties({"playerid": 1, "properties": ["speed"]})["result"] 
+        
+        self.assertEqual(player_properties['speed'], 1) 
+
+        self.xbmc.Player.Stop({"playerid": 1})
+
 if __name__ == '__main__':
     unittest.main()
